3d089269c73e9d8cddaf0e09d9d5e6a12e6d7b09
Jeff Brown <jbrown@candojax.org>
2013-11-02 02:05:36 -0400

    form

--- pages/suadmin_options.php
+++ pages/suadmin_options.php
@@ -1,11 +1,15 @@
 <div class="panel">
 <div class="panel-body">
 <?php 
-print_r($_POST);
+
 $options = $mysqli->query("SELECT * FROM `site_options`");
 while ($option = $options->fetch_assoc()) {
 $opt[$option['optname']] = $option['optval'];
 $options->close();
+}
+
+if(isset($_POST['updateprefs'])) {
+print_r($_POST);
 }
 include('templates/form_site_options.php');
 ?>
