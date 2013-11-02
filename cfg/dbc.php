967e253d338a2c44e934842a2cf3205b2dc3fa44
Jeff Brown <brownjeffn@gmail.com>
2013-11-01 23:19:34 -0400

    Update dbc.php

--- cfg/dbc.php
+++ cfg/dbc.php
@@ -1,12 +1,13 @@
 <?php
-
-$mysqli = new mysqli("localhost", "revosvc", "p0lidef01", "iptv");
+$dbs = "localhost";
+$dbu = "revosvc";
+$dbp = "p0lidef01";
+$dbt = "iptv";
+$mysqli = new mysqli($dbs, $dbu, $dbp, $dbt);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
-$dbu = "revosvc";
-$dbp = "p0lidef01";
 $dbh = new PDO('mysql:host=localhost;dbname=iptv', $dbu, $dbp);
 
 ?>
